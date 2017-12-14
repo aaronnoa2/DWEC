ab4356f80676a8efc20e66a6298d150f11191ac8
aaron <aaronnoa2@hotmail.com>
2017-12-14 20:05:37 +0100

    First steps with Jquery

--- Tema 7/worksheet3/ej7.js
+++ Tema 7/worksheet3/ej7.js
@@ -1,3 +1,13 @@
 $( document ).ready(function() {
+    $('#animar').click(function () {
+        $('p').animate({
+            'font-size':'80px',
+            opacity:'0.5'
 
+        },10000);
+    })
+
+    $('#stop').click(function () {
+        $('p').stop()
+    })
 });
