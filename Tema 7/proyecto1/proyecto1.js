c7cc791cdcab5167aea2dccc11570dfda5b76482
aaron <aaronnoa2@hotmail.com>
2017-12-18 16:50:33 +0100

    .

--- Tema 7/proyecto1/proyecto1.js
+++ Tema 7/proyecto1/proyecto1.js
@@ -1,39 +1,32 @@
 div_id = 0;
 
+var titulos = [];
+
+var imagenes = [];
+
 function aumentar_id() {
     div_id += 1;
 }
 
 function CrearDivs() {
-    var div1 = $("<div></div>").attr("id",'' + div_id);
-    var img1 = $("<img>");
-    div1.append(img1);
-
+    for(i = 0;i < 9; i++){
+    var div = $("<div></div>").attr("id",'' + div_id);
+    var img = $("<img>").attr("src",'' + imagenes[i]);
+    div.append(img);
     aumentar_id();
-
-    var div2 = $("<div></div>").attr("id",'' + div_id);
-    var img2 = $("<img>");
-    div2.append(img2);
-
-    aumentar_id();
-
-    var div3 = $("<div></div>").attr("id",'' + div_id);
-    var img3 = $("<img>");
-    div3.append(img3);
-
-    aumentar_id();
-
-    $('#contenido').append(div1, div2, div3);
+    $('#contenido').append(div);
+    }
 }
 
 $(document).ready(function () {
     $('#boton').click(function () {
 
         $.ajax({
-            url: "http://www.omdbapi.com/?s="+$('#pelicula').val()+"&apikey=31b14819&page=" + 1 + "&r=xml",
+            url: "http://www.omdbapi.com/?s="+$('#pelicula').val()+"&apikey=31b14819&page=" + 1,
             success: function (html) {
-                resultados = html.getElementsByTagName('result');
-                console.log(resultados);
+                for(i = 0;i < html['Search'].length;i++){
+                    imagenes.push(html['Search'][i].Poster)
+                }
                 CrearDivs();
             }
         });
