205dde352055b8131481355805b25ac0ffedc2d9
aaron <aaronnoa2@hotmail.com>
2017-12-19 13:00:19 +0100

    added infinite scroll

--- Tema 7/proyecto1/proyecto1.js
+++ Tema 7/proyecto1/proyecto1.js
@@ -6,9 +6,23 @@ var pagina = 1;
 
 var imagenes = [];
 
+en_peticion = false;
+
 ultimo = 0;
 
-en_peticion = false;
+function resetearVariables() {
+
+    titulos = [];
+
+    pagina = 1;
+
+    imagenes = [];
+
+    ultimo = 0;
+
+    en_peticion = false;
+}
+
 
 function aumentar_id() {
     div_id += 1;
@@ -18,31 +32,37 @@ function CrearDivs(html) {
     for(i = ultimo;i < html['Search'].length; i++){
         imagenes.push(html['Search'][i].Poster);
 
-        var div = $("<div></div>").attr("id",'' + div_id);
+        var div = $("<div></div>").attr("id",'' + div_id).css('text-align','center');
         var img = $("<img>").attr("src",'' + imagenes[i]);
+        var titulo = $('<h2>' + html["Search"][i].Title + '</h2>').css('text-align','center');
 
+        div.append(titulo);
         div.append(img);
         aumentar_id();
         $('#contenido').append(div);
     }
     ultimo = imagenes.length;
-    console.log(ultimo)
+    imagenes = [];
+    titulos = [];
 }
 
 function buscarPelis() {
     $('#boton').click(function () {
 
+        $('#contenido').empty();
+        resetearVariables();
+
         if(en_peticion === false){
             en_peticion = true;
+            filtro = $('#filtrar').val();
             $.ajax({
-            url: "http://www.omdbapi.com/?s="+$('#pelicula').val()+"&apikey=31b14819&page=" + pagina,
+            url: "http://www.omdbapi.com/?s="+$('#pelicula').val()+"&apikey=31b14819&page=" + pagina + '&type=' + filtro + '',
             success: function (html) {
                 CrearDivs(html);
                 pagina += 1;
                 en_peticion = false;
             }
         })}
-
     })
 }
 
@@ -51,18 +71,19 @@ function scrollInfinito() {
 
     win.scroll(function() {
 
-        if ($(document).height() - win.height() <= (win.scrollTop())+ 5 ) {
+        if ($(document).height() - win.height() <= (win.scrollTop() + 800) ) {
             if(en_peticion === false){
+                filtro = $('#filtrar').val();
                 en_peticion = true;
                 $.ajax({
-                url: "http://www.omdbapi.com/?s="+$('#pelicula').val()+"&apikey=31b14819&page=" + pagina,
+                url: "http://www.omdbapi.com/?s="+$('#pelicula').val()+"&apikey=31b14819&page=" + pagina + '&type=' + filtro + '',
                 success: function(html) {
+                    console.log(html);
                     CrearDivs(html);
                     pagina+=1;
                     en_peticion = false;
                 }
             })}
-
         }
     });
 }
