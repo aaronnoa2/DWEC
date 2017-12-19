10749846f58075e5e85ef41465f51383863ccbfc
aaron <aaronnoa2@hotmail.com>
2017-12-19 10:48:41 +0100

    .

--- Tema 7/proyecto1/proyecto1.js
+++ Tema 7/proyecto1/proyecto1.js
@@ -2,33 +2,71 @@ div_id = 0;
 
 var titulos = [];
 
+var pagina = 1;
+
 var imagenes = [];
+
+ultimo = 0;
+
+en_peticion = false;
 
 function aumentar_id() {
     div_id += 1;
 }
 
-function CrearDivs() {
-    for(i = 0;i < 9; i++){
-    var div = $("<div></div>").attr("id",'' + div_id);
-    var img = $("<img>").attr("src",'' + imagenes[i]);
-    div.append(img);
-    aumentar_id();
-    $('#contenido').append(div);
+function CrearDivs(html) {
+    for(i = ultimo;i < html['Search'].length; i++){
+        imagenes.push(html['Search'][i].Poster);
+
+        var div = $("<div></div>").attr("id",'' + div_id);
+        var img = $("<img>").attr("src",'' + imagenes[i]);
+
+        div.append(img);
+        aumentar_id();
+        $('#contenido').append(div);
     }
+    ultimo = imagenes.length;
+    console.log(ultimo)
 }
 
-$(document).ready(function () {
+function buscarPelis() {
     $('#boton').click(function () {
 
-        $.ajax({
-            url: "http://www.omdbapi.com/?s="+$('#pelicula').val()+"&apikey=31b14819&page=" + 1,
+        if(en_peticion === false){
+            en_peticion = true;
+            $.ajax({
+            url: "http://www.omdbapi.com/?s="+$('#pelicula').val()+"&apikey=31b14819&page=" + pagina,
             success: function (html) {
-                for(i = 0;i < html['Search'].length;i++){
-                    imagenes.push(html['Search'][i].Poster)
-                }
-                CrearDivs();
+                CrearDivs(html);
+                pagina += 1;
+                en_peticion = false;
             }
-        });
+        })}
+
     })
+}
+
+function scrollInfinito() {
+    var win = $(window);
+
+    win.scroll(function() {
+
+        if ($(document).height() - win.height() <= (win.scrollTop())+ 5 ) {
+            if(en_peticion === false){
+                en_peticion = true;
+                $.ajax({
+                url: "http://www.omdbapi.com/?s="+$('#pelicula').val()+"&apikey=31b14819&page=" + pagina,
+                success: function(html) {
+                    CrearDivs(html);
+                    pagina+=1;
+                    en_peticion = false;
+                }
+            })}
+
+        }
+    });
+}
+$(document).ready(function () {
+    buscarPelis();
+    scrollInfinito();
 });
